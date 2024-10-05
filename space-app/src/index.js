5ec30f5bd2cc02b61865bf51c0611e30ded00297
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-10-05 10:37:40 -0600

    Changes

--- space-app/src/index.js
+++ space-app/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@mdi/font/css/materialdesignicons.css";
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
 import './index.css';
 import App from './App';
 
