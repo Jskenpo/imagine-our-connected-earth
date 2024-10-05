272d4d675f5dc724f468a94f761ff9794bf26752
Santisteban <san21153@uvg.edu.gt>
2024-10-05 12:50:33 -0600

    creacion de videos, introduccion y primera card

---
+++ space-app/src/components/ClimateCardWhite/ClimateCardWhite.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './ClimateCardWhite.css';
+
+function ClimateCardWhite({ videoSrc, content }) {
+
+    return(
+        <div id='climateCardWhite'>
+            <div id='CCContent'>
+                <p>{content}</p> {/* Muestra el contenido de texto */}
+            </div>
+            <div id='CCVideo'>
+                <video id='CCVideo' autoPlay loop muted>
+                    <source src={videoSrc} type='video/mp4' /> {/* Usa la dirección del video pasada como prop */}
+                </video>
+            </div>
+        </div>
+    )
+}
+
+export default ClimateCardWhite;
--- space-app/src/views/Temperature/temperature.js
+++ space-app/src/views/Temperature/temperature.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Navbar from "../../components/navbar/navbar";
+import "./temperature.css";
+import ClimateCardWhite from "../../components/ClimateCardWhite/ClimateCardWhite";
+import ClimateSpiral from "../../assets/temperature_spiral.mp4";
 
 function Temperature() {
     return (
-        <div>
+        <div id='temperatureView'>
             <Navbar />
-            <h1>Temperature</h1>
+            <div id = 'temperatureViewTitle'>
+                <h1>Climate change through time</h1>
+            </div>
+            <div id = 'temperatureViewIntro'>
+                <p>Climate change is one of the most critical challenges humanity faces in the 21st century. As global temperatures rise and weather patterns shift, our planet is experiencing profound effects on its ecosystems, biodiversity, and human communities. This page aims to educate and raise awareness about the causes, effects, and solutions to mitigate climate change.
+                <br></br>
+                <br></br>
+Through scientific information, updated news, and interactive resources, we will explore how human activities are driving these changes and what actions we can take, both individually and collectively, to reduce environmental impact. Climate change is neither a distant nor abstract issue; it affects everyone, and together we can make a difference.</p>
+            </div>
+            <div id='temperatureViewContent'>
+                <ClimateCardWhite 
+                    videoSrc={ClimateSpiral}
+                    content='' 
+                />
+                
+            </div>
         </div>
     );
 }
